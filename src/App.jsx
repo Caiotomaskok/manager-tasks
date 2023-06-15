6263d579da925a58220c05aa54359166c9dc4ce5
Tomaskok <caiotomaskok078@gmail.com>
2023-06-15 16:53:16 -0300

    feat: add task item

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Tasks from './components/Tasks.jsx'
+
+
+const App = () => {
+  return (
+    <>
+    <Tasks />
+    </>  
+  )
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "../components/tasks.scss"
+
+const Tasks = () => {
+    const [tasks, setTask] = useState([]);
+    
+      const fetchTasks = async () => {
+        try{
+         const {data} = await axios.get ('https://fsc-task-manager-backend.herokuapp.com/tasks')
+         setTask(data)
+        } catch(error){
+          console.log(error)
+        }
+      }
+    
+      useEffect(() =>{
+        fetchTasks();
+      }, [])
+    
+    
+    return (
+        
+        <div className="tasks-container">
+            <h2>Minhas tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Ultimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks.filter(task => task.isCompleted === false)
+                    .map((lastTask) => ( 
+                        <p>{lastTask.description}</p>)
+                        )}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>tarefas Concluidas</h3>
+                <div className="tasks-list">
+                    {tasks.filter((task) => task.isCompleted)
+                    .map((completedTasks) => (
+                        <p>{completedTasks.description}</p>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tasks
