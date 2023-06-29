6d5cc10d2e4c358142ef23c1fe69ced3110de40c
Tomaskok <caiotomaskok078@gmail.com>
2023-06-29 13:22:26 -0300

    feat: add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
 
 import "./App.scss"
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='login' element={<Login />} />
     </Routes>
    
   )
